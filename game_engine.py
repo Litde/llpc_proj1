7dce1282cd784a8237791fa413cd140813461070
Litde <wszieczny.michal.2002@gmail.com>
2025-07-11 21:39:50 +0200

    Update .gitignore

---
+++ game_engine.py
@@ -0,0 +1,23 @@
+
+
+class GameEngine:
+    def __init__(self):
+        self.map_engine = None
+
+
+
+class MapEngine:
+    def generate_seeded_map(self):
+        """Generates a map based on a seeded random number generator."""
+        pass
+
+    def generate_random_map(self):
+        """Generates a map based on a non-seeded random number generator."""
+        pass
+
+    def load_map(self, map_path: str) -> None:
+        """Loads a map from a specified file path."""
+        pass
+
+
+
