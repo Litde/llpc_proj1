6c926e6e0fe814cb639abafd4beedb630a89026b
Litde <wszieczny.michal.2002@gmail.com>
2025-07-16 21:03:01 +0200

    Added missing changes

--- main.py
+++ main.py
@@ -17,9 +17,11 @@ def main():
     # game_engine.map_engine.save_map("test_map.txt")
     game_engine.map_engine.load_map("test_map.txt")
 
-    game_engine.game_logic.populate_entities(num_entities=1000, entity_type=EntityType.ITEM, size=statics.COIN_SIZE, health=0)
+    game_engine.game_logic.populate_entities(num_entities=1000, entity_type=EntityType.ITEM, size=statics.COIN_SIZE)
 
     game_engine.game_logic.populate_entities(num_entities=1000, entity_type=EntityType.ENEMY, size=statics.ENEMY_SIZE, health=100)
+
+    game_engine.game_logic.populate_entities(num_entities=100, entity_type=EntityType.HEALTH, size=statics.ENEMY_SIZE)
 
 
     # game_engine.map_engine.print_map()
